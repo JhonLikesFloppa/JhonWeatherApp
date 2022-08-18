f4e55bcfb22d52fb3ed7fd5217cf0f1eb9df92f8
Jhon <me@jhonlikesfloppa.me>
2022-08-18 16:47:56 +0530

    🚚 | Moved The Web App Into A Folder

--- main.js
+++ main.js
@@ -5,15 +5,17 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    icon: path.join(__dirname, "/ico.ico"),
+    width: 1280,
+    height: 720,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('./src/index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
